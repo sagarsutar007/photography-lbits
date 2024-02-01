9f4d61d796502ef812c149dbef790e0cad711778
SaravakotaBharathi <saravakotabharathi@gmail.com>
2024-02-01 14:54:36 +0530

    feat: changes are done

--- src/components/QrScanner.jsx
+++ src/components/QrScanner.jsx
@@ -27,7 +27,7 @@ const QrScanner = () => {
         className="text-center fs-12 fw-bold"
         style={{ fontFamily: "Inter" }}
       >
-        Scan a Vitl QR
+        Scan a Chroma Cheer QR
       </div>
       <div>
         {showQR ? (
