9dc892afd64ca0f3e097255863e90837520f436c
SaravakotaBharathi <saravakotabharathi@gmail.com>
2024-03-04 20:52:14 +0530

    feat: explore stage1 designs

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = "https://chromagz.com/api/";
-// const BACKEND_URL = "http://localhost/chroma-cheer-b/";
+// const BACKEND_URL = "https://chromagz.com/api/";
+const BACKEND_URL = "http://localhost/chroma-cheer-b/";
 const LOGO_URL ="https://i.postimg.cc/d3YRH2yh/CHROMAGZ-1-1.png";
 const FOOTER_URL="https://i.postimg.cc/rwxVc50V/CHROMAGZ-2.png";
 export { BACKEND_URL,LOGO_URL,FOOTER_URL };
